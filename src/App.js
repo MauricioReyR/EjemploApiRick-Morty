14be9e9ce437efe684e3f9900e1d67a115829edf
Mauricio Rey <mauricioreygmail.com>
2024-02-04 12:46:43 -0500

    API RICK&MORTY de Rafa Tarraga API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
+
+import imagenRickMorty from './img/lcofy7f2.bmp'
 import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
 
 function App() {
+
+  const [characters,setCharacters] = useState(null);
+
+  const reqApi = async () => {
+     const api = await fetch ('https://rickandmortyapi.com/api/character')
+     const characterApi = await api.json();
+     //console.log(characterApi); 
+     //console.log(api);
+     //console.log(characterApi);
+     setCharacters(characterApi.results);
+  };
+  //console.log(characters);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+          {characters ?(
+            <Characters characters={characters} setCharacters={setCharacters}/>
+          ) : (
+            <>
+              <img src={imagenRickMorty} alt="Rick & Morty" className="img-home" />
+              <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+              </>
+          )}
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
